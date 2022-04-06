014bc0d9911d4e5d27f378bc138f133369990c77
An Nguyen Hoang <an2419998@gmail.com>
2022-04-06 17:24:20 +0700

    make todoList

---
+++ README.md
@@ -0,0 +1,14 @@
+## Folder structure
+
+```
+src
+|__ components (shared components between features) | |__ Loading
+| |__ index.jsx
+| |__ styles.scss |
+|__ features
+| |__ Todo
+| |__ components (components of feature Todo)
+| |__ pages (pages of feature Todo)
+| |__ index.jsx (entry point of feature Todo) |
+|__ App.js
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,10 @@
 import { useState } from "react"
-import logo from "./logo.svg"
-import "./App.css"
-import ColorBox from "./ColorBox"
+import TodoFeature from "./features/Todo"
 
 function App() {
-	const name = "Hau"
-	const age = 18
-	const isMale = true
-	const student = {
-		name: "Easy FrontEnd",
-	}
-	const colorList = ["red", "green", "blue"]
-
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} alt="logo" className="App-logo" />
-				<p>Hau Nguyen 123</p>
-				<p>
-					Xin Chao {name} - {age} - {isMale ? "Male" : "Female"}
-				</p>
-				{isMale ? <p>Male</p> : <p>Female</p>}
-				{isMale && <p>Male</p>}
-				{!isMale && <p>Female</p>}
-
-				{isMale && (
-					<div>
-						<p>Male 1</p>
-						<p>Male 2</p>
-						<p>Male 3</p>
-					</div>
-				)}
-
-				{isMale && (
-					<>
-						<p>Male 1</p>
-						<p>Male 2</p>
-						<p>Male 3</p>
-					</>
-				)}
-
-				<p>{student.name}</p>
-				<ul>
-					{colorList.map((color) => (
-						<li style={{ color }}>{color}</li>
-					))}
-				</ul>
-			</header>
+			<TodoFeature />
 		</div>
 	)
 }
