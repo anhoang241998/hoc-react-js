e969e64452b31c12bdef6081861d946a3448d8cf
An Nguyen Hoang <an2419998@gmail.com>
2022-04-07 17:30:16 +0700

    learn basic use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from "react"
-import AlbumFeature from "./features/Album"
-import TodoFeature from "./features/Todo"
+import { useState, useEffect } from "react"
+import PostList from "./components/PostList/PostList"
 
 function App() {
+	const [postList, setPostList] = useState([])
+
+	useEffect(() => {
+		fetchPostList()
+	}, [])
+
+	async function fetchPostList() {
+		try {
+			const requestUrl =
+				"https://js-post-api.herokuapp.com/api/posts?_limit-10&_page=1"
+			const response = await fetch(requestUrl)
+			const responseJSON = await response.json()
+			const { data } = responseJSON
+			setPostList(data)
+		} catch (error) {
+			console.log(`Failed to fetch post list: ${error.message}`)
+		}
+	}
+
 	return (
 		<div className="App">
-			<TodoFeature />
+			<h1>React hooks - PostList</h1>
+			<PostList posts={postList} />
+			{/* <TodoFeature /> */}
 			{/* <AlbumFeature /> */}
 		</div>
 	)
