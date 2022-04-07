12ece27b7a8c72546bd1575e95179ceb9a56a3ee
An Nguyen Hoang <an2419998@gmail.com>
2022-04-07 17:46:20 +0700

    learning advance useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,51 @@
 import { useState, useEffect } from "react"
+import Pagination from "./components/Pagination/Pagination"
 import PostList from "./components/PostList/PostList"
+import queryString from "query-string"
 
 function App() {
 	const [postList, setPostList] = useState([])
+	const [pagination, setPagination] = useState({
+		_page: 1,
+		_limit: 10,
+		_totalRows: 1,
+	})
+	const [filters, setFilters] = useState({
+		_limit: 10,
+		_page: 1,
+	})
 
 	useEffect(() => {
 		fetchPostList()
-	}, [])
+	}, [filters])
 
 	async function fetchPostList() {
 		try {
-			const requestUrl =
-				"https://js-post-api.herokuapp.com/api/posts?_limit-10&_page=1"
+			// _limit=10&_page=3
+			const paramsString = queryString.stringify(filters)
+			const requestUrl = `https://js-post-api.herokuapp.com/api/posts?${paramsString}`
 			const response = await fetch(requestUrl)
 			const responseJSON = await response.json()
-			const { data } = responseJSON
+			const { data, pagination } = responseJSON
 			setPostList(data)
+			setPagination(pagination)
 		} catch (error) {
 			console.log(`Failed to fetch post list: ${error.message}`)
 		}
+	}
+
+	const handlePageChange = (newPage) => {
+		setFilters({
+			...filters,
+			_page: newPage,
+		})
 	}
 
 	return (
 		<div className="App">
 			<h1>React hooks - PostList</h1>
 			<PostList posts={postList} />
+			<Pagination pagination={pagination} onPageChange={handlePageChange} />
 			{/* <TodoFeature /> */}
 			{/* <AlbumFeature /> */}
 		</div>
